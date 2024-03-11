0eee6b264645ad044738f09501c354a19a3e7c8c
Данил <mc.mam1nmodnik@gmail.com>
2024-03-11 17:47:04 +0400

    fix| backend

--- application/backend/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Orders(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    price = models.DecimalField(max_length=100)
--- application/backend/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest framework import serializer
-from .models import User 
-
-class UserSerializer(serializer.ModelSerializer):
-    class Meta: 
-        model = User
-        fields = ['title', 'description', 'price']
-
-
