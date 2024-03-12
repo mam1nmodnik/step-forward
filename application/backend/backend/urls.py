a1351649bdd09ca2ee6d86bd045522bf15da4965
Данил <mc.mam1nmodnik@gmail.com>
2024-03-12 14:47:48 +0400

    fix| backend api

--- application/backend/backend/urls.py
+++ application/backend/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bacend project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls import re_path as url
+from backend_api.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', OrdersView.as_view(), name='orders'),
 ]
--- application/backend/backend_api/models.py
+++ application/backend/backend_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Orders(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
