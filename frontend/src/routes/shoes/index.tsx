a0206c77864734a1b69e3009abd8761ddf7f0d8e
Данил <mc.mam1nmodnik@gmail.com>
2024-04-25 20:31:47 +0400

    feat | adding pagination

--- frontend/src/routes/shoes/index.tsx
+++ frontend/src/routes/shoes/index.tsx
@@ -1,27 +1,50 @@
 
 
-import {  createQuery, QueryClient  } from '@tanstack/solid-query'
-import { For,  Match,Switch, Suspense, onMount, createMemo } from "solid-js";
+import {  createQuery } from '@tanstack/solid-query'
+import { createEffect, createMemo, createSignal, For,  Match,Switch } from "solid-js";
 
 import CardProduct from "~/components/Layouts/Home/HomeContent/newGoods/cardProduct";
 
 import { GETProductAll } from "~/api/api";
 
+import {
+    Pagination,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationItems,
+    PaginationNext,
+    PaginationPrevious
+} from "~/components/ui/pagination"
+import { createMemoryHistory, useParams } from '@solidjs/router';
 
 export default function Shoes(){
     
     const state =  createQuery(() => ({
         queryKey: [`shoes`],
         queryFn: () =>  GETProductAll(),
-      })
+    })
     )
-    createMemo(()=> {
-        console.log(state.data)
+    const interpretLength = (): number => {
+        const length = state.data?.length;   
+        let pages: number = 0;
+        if(length) pages = Math.ceil(length / 12);
+        return pages
+    }
+    
+    const [page, setPage] = createSignal(1);
+    const [array , setArray] = createSignal(state.data)
+    let addContent = 12;
+    
+    createMemo(() => {
+        console.log(page())
+        setArray(state.data?.slice((page() - 1) * addContent, (page() * addContent) ))
+        console.log(array())
     })
     return (
         <div class="lg:mt-[3rem] w-full">
             <div class="rounded-xl ">
-                <div class=" flex flex-wrap justify-between gap-[3rem] ">
+            
+                <div class=" grid grid-cols-4 gap-[3rem] ">
                         <Switch>
                             <Match when={state.isPending}>
                                 <div>Loading....</div>
@@ -30,7 +53,7 @@ export default function Shoes(){
                                 <span>Error: {(state.error as Error).message}</span>
                             </Match>
                             <Match when={state && state.data !== undefined}>
-                                <For each={state.data}>
+                                <For each={array()}>
                                     {(todo) => 
                                         <CardProduct {...todo} /> 
                                     }
@@ -38,6 +61,22 @@ export default function Shoes(){
                             </Match>
                         </Switch>
                 </div>
+                <Pagination class='flex justify-center mt-[3rem]'
+                    onPageChange={setPage}
+                    count={interpretLength()}
+                    itemComponent={(props) => (
+                    <PaginationItem 
+                        page={props.page}          
+                        >    
+                            {props.page}
+                        </PaginationItem>
+                    )}
+                    ellipsisComponent={() => <PaginationEllipsis  />}
+                >
+                    <PaginationPrevious />
+                    <PaginationItems />
+                    <PaginationNext />
+                </Pagination>
             </div>
         </div>
     );
